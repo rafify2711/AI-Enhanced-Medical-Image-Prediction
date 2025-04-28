8fef1e701596f10cc90634a0a9e68c693d5f60d3
Mostafa_Essam <MK.mostafaessam@gmail.com>
2025-04-28 10:43:48 +0300

    add Procfile

--- main.py
+++ main.py
@@ -100,6 +100,9 @@ def read_root():
     return {"message": "API is running ✅"}
 
 # Run the app
-if __name__ == "__main__":
-    import uvicorn
-    uvicorn.run(app, host="127.0.0.1", port=8000) # 127.0.0.1 8000
+# if __name__ == "__main__":
+#     import uvicorn
+#     import os
+
+#     port = int(os.environ.get("PORT", 8000))
+#     uvicorn.run(app, host="0.0.0.0", port=port)  # 127.0.0.1  8000
